1adc6fc482b69ceb062bd692294c47b2f287d22f
Shekhar Bendre <shekharbe@cybage.com>
2018-05-14 19:48:29 +0530

    adding new repo

---
+++ src/app/app.routes.ts
@@ -0,0 +1,15 @@
+import { Routes } from '@angular/router';
+import { HomeComponent } from './home';
+import { AboutComponent } from './about';
+import { EditCustomerCardComponent } from './edit-customer';
+import { NoContentComponent } from './no-content';
+
+export const ROUTES: Routes = [
+  { path: '',      component: HomeComponent },
+  { path: 'home',  component: HomeComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'customer/:id', component: EditCustomerCardComponent },
+  { path: 'detail', loadChildren: './+detail#DetailModule'},
+  { path: 'barrel', loadChildren: './+barrel#BarrelModule'},
+  { path: '**',    component: NoContentComponent },
+];
---
+++ src/app/home/home.component.ts
@@ -0,0 +1,88 @@
+import {
+  Component,
+  OnInit
+} from '@angular/core';
+
+import { NgbTabsetConfig } from '@ng-bootstrap/ng-bootstrap';
+
+import { AppState } from '../app.service';
+import { Title } from './title';
+import { XLargeDirective } from './x-large';
+
+@Component({
+  /**
+   * The selector is what angular internally uses
+   * for `document.querySelectorAll(selector)` in our index.html
+   * where, in this case, selector is the string 'home'.
+   */
+  selector: 'home',  // <home></home>
+  /**
+   * We need to tell Angular's Dependency Injection which providers are in our app.
+   */
+  providers: [
+    Title,
+    NgbTabsetConfig
+  ],
+  /**
+   * Our list of styles in our component. We may add more to compose many styles together.
+   */
+  styleUrls: [ './home.component.css' ],
+  /**
+   * Every Angular template is first compiled by the browser before Angular runs it's compiler.
+   */
+  templateUrl: './home.component.html'
+})
+export class HomeComponent implements OnInit {
+  /**
+   * Set our default values
+   */
+  public localState = { value: '' };
+  private customerList = [];
+  private customerRes;
+  private pageCount = 1;
+  private pageSize = 12;
+  private start = 0;
+  private end = 8;
+  /**
+   * TypeScript public modifiers
+   */
+  constructor(
+    public appState: AppState,
+    public title: Title,
+    config: NgbTabsetConfig,
+  ) {
+    config.justify = 'center';
+    config.type = 'pills';
+    this.customerRes = [];
+  }
+
+  public ngOnInit() {
+    console.log('hello `Home` component');
+    /**
+     * this.title.getData().subscribe(data => this.data = data);
+     */
+    this.getCustomer();
+  }
+
+  public submitState(value: string) {
+    console.log('submitState', value);
+    this.appState.set('value', value);
+    this.localState.value = '';
+  }
+
+  public ngDoCheck() {
+    this.start = this.pageSize * (this.pageCount - 1);
+    this.end = this.pageSize * this.pageCount;
+    this.customerList = this.customerRes.slice(this.start, this.end);
+  }
+
+  /**
+   * ----------------------------------------------------
+   * Get All Users
+   * ----------------------------------------------------
+   * @method getCustomer
+   */
+  public getCustomer() {
+    this.title.getCustomerData().subscribe((data) => this.customerRes = data );
+  }
+}
